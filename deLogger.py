79f60df11402c3b0b611e157d0c3a0874863ffa2
DANNY <danykhosravi@gmail.com>
2018-07-08 11:26:22 +0430

    3rd dimens is ok

--- deLogger.py
+++ deLogger.py
@@ -1,13 +1,23 @@
 grades = []
-demX=[]
-Token-0
-XVector=1
+
+Tokens=0
+xVector=1
 yVector=2
 zVector=3
 steps=4
 with open('readlog.txt',"r") as f:
     variable=f.readlines()
 #--------------------
+
+
+def CreateDimen(Vector,dataInput,Steps):
+    dem=[]
+    for x in range(Vector,len(dataInput),Steps):
+        dem.append(dataInput[x])
+    return dem,len(dem)   
+
+
+
 for i in range(len(variable)):
     grades.append(variable[i].strip('\n'))
 print(grades)
@@ -17,12 +27,16 @@ newgrades = list(filter(lambda x : x != '222222', grades))
 print("ruslaka")
 
 print(newgrades)
-o=0
-for x in range(1,len(newgrades),4):
-    
-    demX.append(newgrades[x])
-    # print(newgrades[x]) 
-print(demX)    
+print("token=>")
+print(CreateDimen(Tokens,newgrades,steps))
+print("X=>")
+print(CreateDimen(xVector,newgrades,steps))
+print("Y=>")
+print(CreateDimen(yVector,newgrades,steps))
+print("Z=>")
+print(CreateDimen(zVector,newgrades,steps))
+   
 
-def CreateDimen(Vector,dataInput,Steps):
+
+
     
