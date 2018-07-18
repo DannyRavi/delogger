a7e9c37e78ae80d8ffdf69700f4b2b5696028c57
DANNY <danykhosravi@gmail.com>
2018-07-18 17:03:12 +0430

    Create output file & refourm data mining

--- deLogger.py
+++ deLogger.py
@@ -1,8 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
 from  dimes import *
 
 grades = []
-eliminate_Item='222222'
-with open('readlog.txt',"r") as f:
+TokenDim=[]
+Xdim=[]
+Ydim=[]
+Zdim=[]
+Tdim=[]
+results=[]
+eliminate_Item='22222'
+plt.close()
+with open('ForwardStop.txt',"r") as f:
     variable=f.readlines()
 #--------------------
 
@@ -12,24 +21,67 @@ for i in range(len(variable)):
 
 
 newgrades = list(filter(lambda x : x != eliminate_Item, grades))
-print("allItem=>\n")
-print(newgrades)
-print("\n \n")
+TokenDim=CreateDimen(Tokens,newgrades,steps)
+Xdim=CreateDimen(xVector,newgrades,steps)
+Ydim=CreateDimen(yVector,newgrades,steps)
+Zdim=CreateDimen(zVector,newgrades,steps)
+Tdim=CreateDimen(tVector,newgrades,steps)
+
+
+CreateFile(Xdim,Ydim,Zdim,Tdim)
+
+
+Xdim = list(map(int, Xdim))
+Ydim = list(map(int, Ydim))
+Zdim = list(map(int, Zdim))
+
+
+Xdim = np.abs(Xdim)
+Ydim = np.abs(Ydim)
+Zdim = np.abs(Zdim)
+
+
+# print("allItem=>\n")
+# print(newgrades)
+# print("\n \n")
 print("token=>\n")
 print(CreateDimen(Tokens,newgrades,steps))
-print("\n \n")
-print("X=>\n")
-print(CreateDimen(xVector,newgrades,steps))
-print("\n \n")
-print("Y=>\n")
-print(CreateDimen(yVector,newgrades,steps))
-print("\n \n")
-print("Z=>\n")
-print(CreateDimen(zVector,newgrades,steps))
-print("\n \n")
+# print("\n \n")
+# print("X=>\n")
+# print(CreateDimen(xVector,newgrades,steps))
+# print("\n \n")
+# print("Y=>\n")
+# print(CreateDimen(yVector,newgrades,steps))
+# print("\n \n")
+# print("Z=>\n")
+# print(CreateDimen(zVector,newgrades,steps))
+# print("\n \n")
 print("t=>\n")
 print(CreateDimen(tVector,newgrades,steps))
 
+
+
+plt.figure(1)
+
+plt.subplot(311)
+plt.plot(Xdim)
+plt.title('X Dimen')
+plt.grid(True)
+
+plt.subplot(312)
+plt.plot(Ydim)
+plt.ylabel('Amp')
+plt.title('Y Dimen')
+plt.grid(True)
+
+plt.subplot(313)
+plt.plot(Zdim)
+plt.xlabel('Index')
+plt.title('Z Dimen')
+plt.grid(True)
+
+plt.show()
+plt.close()
 
 
     
--- dimes.py
+++ dimes.py
@@ -12,4 +12,21 @@ def CreateDimen(Vector,dataInput,Steps):
     dem=[]
     for x in range(Vector,len(dataInput),Steps):
         dem.append(dataInput[x])
-    return dem,len(dem)  
+    return dem 
+
+
+def CreateFile(X,Y,Z,T):
+    f= open("outPutDimen.txt","w+")
+    f.write(" X=> \r\n")
+    f.write(str(X))
+    f.write("\r\n")
+    f.write(" Y=> \r\n")
+    f.write(str(Y))
+    f.write("\r\n")
+    f.write(" Z=> \r\n")
+    f.write(str(Z))
+    f.write("\r\n")
+    f.write(" T=> \r\n")
+    f.write(str(T))
+    f.write("\r\n")
+    f.close() 
