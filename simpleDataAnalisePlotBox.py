073a9f534e1987eb1d219618dc9bec375557ffa1
DANNY <danykhosravi@gmail.com>
2018-11-06 15:37:39 +0330

    rename a file

--- simpleDataAnalisePlotBox.py
+++ simpleDataAnalisePlotBox.py
@@ -95,8 +95,12 @@ b = pd.DataFrame({ 'group' :"Empty", 'value': Fy })
 df=a.append(b)
  
 # Usual boxplot
-sb.boxplot(x='group', y='value', data=df)
+ax = sb.boxplot(x='group', y='value', data=df)
 
 
+ax = sb.stripplot(x='group', y='value', data=df, color="red", jitter=0.2, size=2.5)
 
+plt.title("Boxplot with jitter", loc="left")
+
+# sb.distplot(Fy)
 plt.show()
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,1 +1,6 @@
-py
+x = sb.boxplot(x='group', y='value', data=df)
+
+
+# ax = sb.stripplot(x='group', y='value', data=df, color="red", jitter=0.2, size=2.5)
+
+# plt.title("Boxplot with jitter", loc="left")
